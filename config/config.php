48a47fe78f202e7f85c5b68db236addaf83b3d8b
iklagan00 <201980202@psu.palawan.edu.ph>
2021-11-04 20:07:56 +0800

    changes made

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 	define('ROOT_URL', 'https://infinite-spire-63641.herokuapp.com/');
 	define('DB_HOST', 'sql6.freesqldatabase.com');
-	define('DB_USER', 'sql6447569');
-	define('DB_PASS', 'PwgfFD6W9A');
-	define('DB_NAME', 'sql6447569');
+	define('DB_USER', 'sql6448921');
+	define('DB_PASS', 'aD6McwN8Xk');
+	define('DB_NAME', 'sql6448921');
